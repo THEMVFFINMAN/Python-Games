7b755fd9e9e7a6265971450aaf94801d7f2e19f7
JJ <joshuajordanlowe@gmail.com>
2016-10-01 21:51:05 -0400

    7261 points scored

--- Codingame.com/accountant.py
+++ Codingame.com/accountant.py
@@ -24,7 +24,7 @@ while True:
     enemy_count = int(input())
     for i in range(enemy_count):
         enemy_id, enemy_x, enemy_y, enemy_life = [int(j) for j in input().split()]
-        enemies[enemy_id] = (enemy_x, enemy_y, enemy_life, 0, 0)
+        enemies[enemy_id] = [enemy_x, enemy_y, enemy_life, 0, 0]
         
         
     data_distance.clear()
@@ -35,14 +35,22 @@ while True:
         enemy_distance_dict[enemy_key] = enemy_distance_for_one
         enemy_distance_list.append(enemy_distance_for_one)
         
-        data_distances_for_one_enemy = []
+        closest_data_to_enemy = 100000000
+        closest_data_x = 0
+        closest_data_y = 0
         
         for data_key in data:
-            data_distances_for_one_enemy.append((math.hypot(data[data_key][0] - enemies[enemy_key][0], data[data_key][1] - enemies[enemy_key][1]), data_key))
+            distance_to_data_point = math.hypot(data[data_key][0] - enemies[enemy_key][0], data[data_key][1] - enemies[enemy_key][1])
             
-        data_distance[enemy_key] = min(data_distances_for_one_enemy,key=itemgetter(1))
-        print (min(data_distances_for_one_enemy,key=itemgetter(1)), file=sys.stderr)
-    #    for j in range(enemy_count):
+            if distance_to_data_point < closest_data_to_enemy:
+                closest_data_to_enemy = distance_to_data_point
+                closest_data_x = data[data_key][0]
+                closest_data_y = data[data_key][1]
+            
+        data_distance[enemy_key] = closest_data_to_enemy
+        
+        enemies[enemy_key][3] = closest_data_x
+        enemies[enemy_key][4] = closest_data_y
     
     index_of_enemy_to_data = min(data_distance, key=data_distance.get)
     data_distance_to_enemy = data_distance[index_of_enemy_to_data]
@@ -50,26 +58,34 @@ while True:
     index_of_enemy_to_player = min(enemy_distance_dict, key=enemy_distance_dict.get)
     enemy_distance_to_player = enemy_distance_dict[index_of_enemy_to_player]
     
-    if enemy_distance_to_player < 2500:    
+    if enemy_distance_to_player < 3000:    
         enemy = enemies[index_of_enemy_to_player]
         enemy_x = enemy[0]
         enemy_y = enemy[1]
+        data_x = enemy[3]
+        data_y = enemy[4]
+        user_distance_to_data = math.hypot(x - data_x, y - data_y)
+        close_enemy_distance_to_data = math.hypot(enemy_x - data_x, enemy_y - data_y)
+        print(user_distance_to_data, enemy_distance_to_player, file=sys.stderr)
         
-        if x > 15600:
-            print("some poop", file=sys.stderr)
-            x_to_move = x
-            if y > enemy_y:
-                y_to_move = y + 500
-            else:
-                y_to_move = y - 500
-        else:
+        if user_distance_to_data < close_enemy_distance_to_data:
             x_to_move = x + (x - enemy_x)
             y_to_move = y + (y - enemy_y)
+            
+            if x_to_move > 16000:
+                x_to_move = 16000
+            if y_to_move > 9000:
+                y_to_move = 9000
+            
+            print(x_to_move, y_to_move, x, y, file=sys.stderr)
+            if abs(x - x_to_move) + abs(y - y_to_move) < 500:
+                print("done fucked up", file=sys.stderr)
+            
         
-    
-        print("MOVE {} {}".format(x_to_move, y_to_move))
+            print("MOVE {} {}".format(x_to_move, y_to_move))
+            continue
+
+    if enemy_distance_to_player - 3500 < data_distance_to_enemy: 
+        print("SHOOT {}".format(index_of_enemy_to_player))
     else:
-        if enemy_distance_to_player < data_distance_to_enemy[0]: 
-            print("SHOOT {}".format(index_of_enemy_to_player))
-        else:
-            print("SHOOT {}".format(index_of_enemy_to_data))
+        print("SHOOT {}".format(index_of_enemy_to_data))
